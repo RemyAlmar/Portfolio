165999dae5687c369051b458aa64858f9864d842
RemyAlmar <r.almar1997@gmail.com>
2024-11-17 14:12:47 +0100

    [ADD] Workspace

--- main.js
+++ main.js
@@ -11,6 +11,17 @@ let CreateJobCard = function(data)
                     Your browser does not support HTML5 video.
                 </video>
                 <div class="video-additional-info">
+                
+                    <div class="project-Title">
+                        <h3 class="clickable-card-header">${data.title}
+                            <svg class="svg-inline--fa fa-chevron-right clickable-header-icon" aria-hidden="true" focusable="true" data-prefix="fas" data-icon="chevron-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                                <path class="" fill="currentColor"
+                                    d="M96 480c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L242.8 256L73.38 86.63c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25l-192 192C112.4 476.9 104.2 480 96 480z">
+                                </path>
+                            </svg>
+                        </h3>
+                    </div>
+
                     <div class="team-size icon-container">
                         <svg class="svg-inline--fa fa-users icon" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="users" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
                             <path class="" fill="currentColor"
@@ -35,16 +46,10 @@ let CreateJobCard = function(data)
                         </svg>
                         <p>${data.codeUsed}</p>
                     </div>
-                </div>
+                </div>     
             </div>
-            <div class="project-description">
-                <h3 class="clickable-card-header">${data.title}
-                    <svg class="svg-inline--fa fa-chevron-right clickable-header-icon" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
-                        <path class="" fill="currentColor"
-                            d="M96 480c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L242.8 256L73.38 86.63c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25l-192 192C112.4 476.9 104.2 480 96 480z">
-                        </path>
-                    </svg></h3>
-                <small>${data.role}</small>
+            <div class="project-description">           
+                <small>My Role : ${data.role}</small>
                 <p>${data.description}</p>
             </div>
     `;
